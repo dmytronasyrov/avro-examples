71de9c956c120cf6127d235579214269787ea5a3
Dmytro Nasyrov <dmytro@pharosproduction.com>
2020-01-06 22:28:08 +0200

    add generic record from string example

--- src/main/java/com/pharosproduction/avro/generic/GenericRecordExamples.java
+++ src/main/java/com/pharosproduction/avro/generic/GenericRecordExamples.java
@@ -1,8 +1,37 @@
 package com.pharosproduction.avro.generic;
 
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecordBuilder;
+
 public class GenericRecordExamples {
 
   public static void main(String[] args) {
+    final Schema.Parser parser = new Schema.Parser();
+    final Schema schema = parser.parse("{\n" +
+      "     \"type\": \"record\",\n" +
+      "     \"namespace\": \"com.example\",\n" +
+      "     \"name\": \"Customer\",\n" +
+      "     \"doc\": \"Avro Schema for our Customer\",     \n" +
+      "     \"fields\": [\n" +
+      "       { \"name\": \"first_name\", \"type\": \"string\", \"doc\": \"First Name of Customer\" },\n" +
+      "       { \"name\": \"last_name\", \"type\": \"string\", \"doc\": \"Last Name of Customer\" },\n" +
+      "       { \"name\": \"age\", \"type\": \"int\", \"doc\": \"Age at the time of registration\" },\n" +
+      "       { \"name\": \"height\", \"type\": \"float\", \"doc\": \"Height at the time of registration in cm\" },\n" +
+      "       { \"name\": \"weight\", \"type\": \"float\", \"doc\": \"Weight at the time of registration in kg\" },\n" +
+      "       { \"name\": \"automated_email\", \"type\": \"boolean\", \"default\": true, \"doc\": \"Field indicating if the user is enrolled in marketing emails\" }\n" +
+      "     ]\n" +
+      "}");
+
+    GenericRecordBuilder customerBuilder = new GenericRecordBuilder(schema);
+    customerBuilder.set("first_name", "John");
+    customerBuilder.set("last_name", "Doe");
+    customerBuilder.set("age", 25);
+    customerBuilder.set("height", 170f);
+    customerBuilder.set("weight", 80.5f);
+    customerBuilder.set("automated_email", false);
+    final GenericData.Record customer = customerBuilder.build();
+    System.out.println(customer);
 
   }
 }
