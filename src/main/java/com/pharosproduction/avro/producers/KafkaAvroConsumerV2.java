350cb8094fc8b43f2d9b1545361fe5f5d629880d
Dmytro Nasyrov <dmytro@pharosproduction.com>
2020-01-18 10:50:05 +0200

    add avro v1,v2 producers,consumers

--- src/main/java/com/pharosproduction/avro/producers/KafkaAvroConsumerV2.java
+++ src/main/java/com/pharosproduction/avro/producers/KafkaAvroConsumerV2.java
@@ -1,6 +1,6 @@
 package com.pharosproduction.avro.producers;
 
-import com.pharosproduction.avro.CustomerV1;
+import com.pharosproduction.avro.CustomerV2;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -13,12 +13,12 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
-public class KafkaAvroConsumerV1 {
+public class KafkaAvroConsumerV2 {
 
   public static void main(String[] args) {
     final Properties properties = new Properties();
     properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-    properties.setProperty("group.id", "customer-consumer-group-v1");
+    properties.setProperty("group.id", "customer-consumer-group-v2");
     properties.setProperty("auto.commit.enable", "false");
     properties.setProperty("auto.offset.reset", "earliest");
     properties.setProperty("acks", "1");
@@ -30,15 +30,15 @@ public class KafkaAvroConsumerV1 {
     properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
     properties.setProperty("specific.avro.reader", "true");
 
-    final KafkaConsumer<String, CustomerV1> consumer = new KafkaConsumer<>(properties);
+    final KafkaConsumer<String, CustomerV2> consumer = new KafkaConsumer<>(properties);
     consumer.subscribe(Collections.singleton("customer-avro"));
 
     System.out.println("Waiting for data...");
 
     while (true) {
-      final ConsumerRecords<String, CustomerV1> records = consumer.poll(Duration.ofMillis(500));
+      final ConsumerRecords<String, CustomerV2> records = consumer.poll(Duration.ofMillis(500));
 
-      for (ConsumerRecord<String, CustomerV1> record : records) {
+      for (ConsumerRecord<String, CustomerV2> record : records) {
         System.out.println("Customer: " + record.value());
       }
 
--- src/main/java/com/pharosproduction/avro/producers/KafkaAvroProducerV2.java
+++ src/main/java/com/pharosproduction/avro/producers/KafkaAvroProducerV2.java
@@ -1,6 +1,6 @@
 package com.pharosproduction.avro.producers;
 
-import com.pharosproduction.avro.CustomerV1;
+import com.pharosproduction.avro.CustomerV2;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -10,12 +10,12 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
-public class KafkaAvroProducerV1 {
+public class KafkaAvroProducerV2 {
 
   public static void main(String[] args) {
     final Properties properties = new Properties();
     properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-    properties.setProperty("group.id", "customer-consumer-group-v1");
+    properties.setProperty("group.id", "customer-consumer-group-v2");
     properties.setProperty("auto.commit.enable", "false");
     properties.setProperty("auto.offset.reset", "earliest");
     properties.setProperty("key.serializer", StringSerializer.class.getName());
@@ -24,18 +24,19 @@ public class KafkaAvroProducerV1 {
     properties.setProperty("value.deserializer", KafkaAvroDeserializer.class.getName());
     properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
 
-    final KafkaProducer<String, CustomerV1> kafkaProducer = new KafkaProducer<>(properties);
+    final KafkaProducer<String, CustomerV2> kafkaProducer = new KafkaProducer<>(properties);
     final String topic = "customer-avro";
 
-    final CustomerV1.Builder customerBuilder = CustomerV1.newBuilder()
+    final CustomerV2.Builder customerBuilder = CustomerV2.newBuilder()
       .setFirstName("John")
       .setLastName("Doe")
       .setAge(25)
       .setWeight(80.5f)
       .setHeight(177f)
-      .setAutomatedEmail(false);
-    final CustomerV1 customer = customerBuilder.build();
-    final ProducerRecord<String, CustomerV1> producerRecord = new ProducerRecord<>(topic, customer);
+      .setEmail("aaa@bbb.com")
+      .setPhoneNumber("123-123-123-1");
+    final CustomerV2 customer = customerBuilder.build();
+    final ProducerRecord<String, CustomerV2> producerRecord = new ProducerRecord<>(topic, customer);
 
     kafkaProducer.send(producerRecord, (recordMetadata, e) -> {
       if (e != null)
